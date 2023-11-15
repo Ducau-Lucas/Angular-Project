340bfc399c328359855823d6c389cab26a968b5e
Ducau.Lucas <lucas.ducau64@gmail.com>
2023-11-15 16:01:05 +0100

    Slideshow Banner and Partners (Meme Edition)

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  images = ['../../assets/image1.jpg', '../../assets/image2.jpg'];
+  currentSlide = 0;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlide = ++this.currentSlide % this.images.length;
+    }, 5000)
+  }
 }
